7dfca69099561a683304e0fe5c51bab08db206b2
TopAcademyBel <121047497+TopAcademy2022@users.noreply.github.com>
2025-02-18 06:08:31 +0300

    Added network udp plugin. Closed #13 (#14)

---
+++ network-udp/NetworkUdp/NetworkUdpPlugin.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using TopChat.API.Entities;
+using TopChat.API.Interfaces;
+
+namespace NetworkUdp
+{
+    public class NetworkUdpPlugin : IPlugin
+    {
+        public string GetName()
+        {
+            return "NetworkUdp";
+        }
+
+        public string GetVersion()
+        {
+            return "0.1";
+        }
+
+        public List<PluginRealization> GetRealizations()
+        {
+            List<PluginRealization> pluginRealizations = new List<PluginRealization>();
+
+            PluginRealization connectionProvider = new PluginRealization()
+            {
+                InterfaceName = "TopChat.API.Interfaces.IConnectionProvider",
+                RealizationClassName = "NetworkUdp.Services.ConnectionProviderUdp"
+            };
+
+
+            pluginRealizations.Add(connectionProvider);
+
+            return pluginRealizations;
+        }
+
+        public void Invoke(){}
+    }
+}
---
+++ network-udp/NetworkUdp/Services/ConnectionProviderUdp.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using TopChat.API.Interfaces;
+
+namespace NetworkUdp.Services
+{
+    public class ConnectionProviderUdp : IConnectionProvider
+    {
+        private readonly UdpClient _udpClient;
+
+        private int _port;
+
+        private const int _DEFAULT_PORT = 5050;
+
+        public ConnectionProviderUdp()
+        {
+            _port = _DEFAULT_PORT;
+            _udpClient = new UdpClient();
+        }
+
+        public void SetPort(int port)
+        {
+            _port = port;
+        }
+
+        public bool Send(byte[] data, string destinationIp, int destinationPort)
+        {
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(destinationIp), destinationPort);
+            if (_udpClient.Send(data, data.Length, iPEndPoint) > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public byte[] Receive()
+        {
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, _port);
+            return _udpClient.Receive(ref iPEndPoint);
+        }
+    }
+}
